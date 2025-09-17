c8c6234f8832306c0ffd2200866a323f46125306
Chase <st10433939@vcconnect.edu.za>
2025-09-17 11:48:55 +0200

    Add project files.

---
+++ 10433939_PROG6212_POE_P1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _10433939_PROG6212_POE_P1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
