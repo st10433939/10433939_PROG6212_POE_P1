d061ae61ef8353909095a9dfa8d6440d7ffccf4b
Chase <st10433939@vcconnect.edu.za>
2025-10-21 09:26:04 +0200

    Added empty models and Data folder

---
+++ 10433939_PROG6212_POE_P1/Data/ClaimData.cs
@@ -0,0 +1,12 @@
+﻿using AspNetCoreGeneratedDocument;
+using _10433939_PROG6212_POE_P1.Models;
+
+namespace _10433939_PROG6212_POE_P1.Data
+{
+    public class ClaimData
+    {
+        
+    }
+}
+
+    
---
+++ 10433939_PROG6212_POE_P1/Models/Claim.cs
@@ -0,0 +1,17 @@
+﻿namespace _10433939_PROG6212_POE_P1.Models
+{
+    public class Claim
+    {
+        public int Id { get; set; }
+        public int HoursWorked { get; set; }
+        public int HourlyRate { get; set; }
+        public string AdditionalNotes { get; set; }
+        public DateTime SubmittedDate { get; set; }
+        public string SubmittedBy { get; set; }
+        public ClaimStatus Status { get; set; }
+        public string ReviewedBy { get; set; }
+        public DateTime ReviewedDate { get; set; }
+        public List<UploadedDocument> Documents { get; set; }
+        public List<ClaimReview> Reviews { get; set; } = new List<ClaimReview>();
+    }
+}
